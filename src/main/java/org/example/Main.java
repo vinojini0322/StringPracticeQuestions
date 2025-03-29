472f301eb46861fc107c9e1418638a847ec71398
vinojiniParamasivam <vinojini.p@viryaconsulting.com>
2025-03-29 11:55:23 +0530

    Leetcode 1816

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
     }
 
+    //    Leetcode 125
     private static boolean isPalindrome(String word) {
         word.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
         for (int i = 0; i < word.length() / 2; i++) {
@@ -14,6 +15,7 @@ public class Main {
         return true;
     }
 
+    //   Leetcode 1662
     private boolean arrayStringsAreEqual(String[] word1, String[] word2) {
         StringBuilder s1 = new StringBuilder();
         StringBuilder s2 = new StringBuilder();
@@ -25,6 +27,18 @@ public class Main {
             s2.append(s.toLowerCase());
         }
         return s1.compareTo(s2) == 0;
+    }
+
+// Leetcode 1816
+
+    private static String truncateSentence(String s, int k) {
+        String[] words = s.split(" ");
+        StringBuilder truncated = new StringBuilder();
+        for (int i = 0; i < k-1; i++) {
+            truncated.append(words[i]).append(" ");
+        }
+        truncated.append(words[k-1]);
+        return truncated.toString();
     }
 
 }
